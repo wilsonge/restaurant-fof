65d7f5c65b7a7b7d7c93734ec3539e121d8c6226
wilsonge <georgejameswilson@googlemail.com>
2013-09-15 15:41:04 +0100

    Use a form.php file to render the view
    this is in anticipation of using HMVC for a comments and comment view

---
+++ com_reviews/site/views/restaurant/tmpl/form.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @copyright (C) 2013 JoomJunk. All rights reserved.
+ * @package    Restaurant Reviews
+ * @license    http://www.gnu.org/licenses/gpl-3.0.html
+ **/
+
+// Get the restaurant view from the XML file
+$viewTemplate = $this->getRenderedForm();
+echo $viewTemplate;
