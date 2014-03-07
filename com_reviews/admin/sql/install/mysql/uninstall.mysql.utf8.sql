a2781cba43b4de9135226736fe9f25b43c4f8ed2
George Wilson <georgejameswilson@googlemail.com>
2014-03-07 03:37:11 +0000

    Tags Support

--- com_reviews/admin/sql/install/mysql/uninstall.mysql.utf8.sql
+++ com_reviews/admin/sql/install/mysql/uninstall.mysql.utf8.sql
@@ -1,2 +1,4 @@
 DROP TABLE IF EXISTS `#__reviews_restaurants`;
 DROP TABLE IF EXISTS `#__reviews_comments`;
+
+DELETE FROM `#__content_types` WHERE `type_alias` = 'com_reviews.restaurant';
---
+++ com_reviews/admin/tables/restaurant.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @copyright (C) 2013 JoomJunk. All rights reserved.
+ * @package    Restaurant Reviews
+ * @license    http://www.gnu.org/licenses/gpl-3.0.html
+ **/
+
+defined('_JEXEC') or die();
+
+class ReviewsTableRestaurant extends FOFTable
+{
+	/**
+	 * Public constructor
+	 *
+	 * @param   array  $config  The configuration variables
+	 */
+	public function __construct($table, $key, &$db, $config = array())
+	{
+		if (!isset($config['behaviors'])) {
+			$config['behaviors'] = array('tags');
+		}
+		else
+		{
+			$config['behaviors'] = array_merge(array('tags'), (array) $config['behaviors']);
+		}
+		
+		self::setHasTags(true);
+
+		parent::__construct($table, $key, $db, $config);
+	}
+}
