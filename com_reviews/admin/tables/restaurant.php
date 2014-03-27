9b764ffef237306482c0fbd7dbdd9f81e1215295
George Wilson <georgejameswilson@googlemail.com>
2014-03-27 18:50:58 +0000

    Refactor setting content type

--- com_reviews/admin/tables/restaurant.php
+++ com_reviews/admin/tables/restaurant.php
@@ -12,13 +12,6 @@ include_once JPATH_LIBRARIES.'/fof/include.php';
 class ReviewsTableRestaurant extends FOFTable
 {
 	/**
-	 * The content type of the table. Required if using tags or content history behaviour
-	 * 
-	 * @var  string
-	 */
-	protected $contentType = null;
-
-	/**
 	 * Public constructor
 	 *
 	 * @param   array  $config  The configuration variables
@@ -34,9 +27,9 @@ class ReviewsTableRestaurant extends FOFTable
 		}
 		
 		self::setHasTags(true);
-		
-		$this->contentType = 'com_reviews.restaurant';
 
 		parent::__construct($table, $key, $db, $config);
+
+		$this->contentType = 'com_reviews.restaurant';
 	}
 }
