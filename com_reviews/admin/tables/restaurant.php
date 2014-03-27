2147ecfa23f1c9ad56f54f46026097fcb1475a14
George Wilson <georgejameswilson@googlemail.com>
2014-03-27 16:49:08 +0000

    Add the contentType

--- com_reviews/admin/tables/restaurant.php
+++ com_reviews/admin/tables/restaurant.php
@@ -12,6 +12,13 @@ include_once JPATH_LIBRARIES.'/fof/include.php';
 class ReviewsTableRestaurant extends FOFTable
 {
 	/**
+	 * The content type of the table. Required if using tags or content history behaviour
+	 * 
+	 * @var  string
+	 */
+	protected $contentType = null;
+
+	/**
 	 * Public constructor
 	 *
 	 * @param   array  $config  The configuration variables
@@ -27,6 +34,8 @@ class ReviewsTableRestaurant extends FOFTable
 		}
 		
 		self::setHasTags(true);
+		
+		$this->contentType = 'com_reviews.restaurant';
 
 		parent::__construct($table, $key, $db, $config);
 	}
